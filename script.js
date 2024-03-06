5fb3e19cba243113379e1e16e4f4aa0e338256a2
Domen <domen.sinkovec@gmail.com>
2024-03-06 18:01:09 +0100

    new

--- script.js
+++ script.js
@@ -11,28 +11,32 @@ const weatherIcon = document.querySelector(".weather-icon");
 //*ASYNC FNC TO CALL API TO GET RESPONSE
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-  let data = await response.json();
+  //*DISPLAY ERROR MESSAGE
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+  } else {
+    let data = await response.json();
+    console.log(data);
 
-  //*DOM ELEMENTS*/
-  document.querySelector(".city").innerHTML = data.name;
-  document.querySelector(".temp").innerHTML =
-    Math.round(data.main.temp) + " °C";
-  document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
-  document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    //*DOM ELEMENTS*/
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + " °C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
-  //*WEATHER IMAGES DISPLAY*/
-  if (data.weather[0].main == "Clouds") {
-    weatherIcon.src = "images/clouds.png";
-  } else if (data.weather[0].main == "Clear") {
-    weatherIcon.src = "images/clear.png";
-  } else if (data.weather[0].main == "Rain") {
-    weatherIcon.src = "images/rain.png";
-  } else if (data.weather[0].main == "Drizzle") {
-    weatherIcon.src = "images/drizzle.png";
-  } else if (data.weather[0].main == "Clear") {
-    weatherIcon.src = "images/clear.png";
-  } else if (data.weather[0].main == "Mist") {
-    weatherIcon.src = "images/mist.png";
+    //*WEATHER IMAGES DISPLAY*/
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "images/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "images/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "images/mist.png";
+    }
   }
 }
 
@@ -42,6 +46,15 @@ searchBtn.addEventListener("click", () => {
 });
 
 //* Function to get weather based on current location
+/* When the page loads, the getWeatherByLocation() function is called. This function is responsible for obtaining the user's current location.
+The getCurrentLocation() function is called within getWeatherByLocation(). This function utilizes the navigator.geolocation.getCurrentPosition() 
+method to retrieve the current position asynchronously.
+If the user grants permission for location access, the browser provides the latitude and longitude coordinates through the getCurrentPosition()
+ function's success callback.
+These coordinates are then passed to the OpenWeatherMap API to fetch the weather data for the user's current location.
+If the user denies permission or if there's any error in retrieving the location, an error message is logged to the console, and the user can manually 
+search for a location using the input box.
+So, in summary, the code uses the Geolocation API provided by the browser to obtain the user's current location coordinates.*/
 async function getWeatherByLocation() {
   try {
     const position = await getCurrentLocation();
